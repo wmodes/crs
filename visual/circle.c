5f7b24a34070b196ab6f7c0d97e14d73db7e34db
Wes Modes <wmodes@gmail.com>
2014-02-24 17:35:36 -0800

    housekeeping

--- visual/circle.c
+++
@@ -1,7 +0,0 @@
-import sys
-r = int(sys.argv[1])
- 
- for y in xrange(-r*1.3,r*1.3,2):
-     for x in xrange(-r*1.3,r*1.3):
-	 sys.stdout.write('#$@%&0*;:,. '[min(abs(r*r - (x*x+y*y))/(r/2), 11)])
-     print

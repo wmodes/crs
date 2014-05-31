2bd8b011d8e2532cd1c404ad7cc435087e5b9d6e
Brent Townshend <bst@tc.com>
2014-05-31 00:28:01 -0700

    Bracket cells, connections with begin/end pairs

--- visual/mydataelements.py
+++ visual/mydataelements.py
@@ -30,6 +30,7 @@ from graphelements import Circle, Line
 
 # constants
 LOGFILE = config.logfile
+OSCPATH=config.oscpath
 
 DEF_DIAM = config.default_diam
 DEF_LINECOLOR = config.default_linecolor
@@ -80,6 +81,7 @@ class MyCell(Cell):
                                 self.m_diam/2, 
                                 color=self.m_color, 
                                 solid=False)
+            self.m_field.m_osc.send_laser(OSCPATH['graph_begin'],["cell",self.m_id])
             self.m_shape.draw()
             if DRAW_BODIES:
                 self.m_bodyshape.update(self.m_field, (self.m_x, self.m_y),
@@ -87,6 +89,7 @@ class MyCell(Cell):
                                           color=self.m_body_color,
                                           solid=True)
                 self.m_bodyshape.draw()
+            self.m_field.m_osc.send_laser(OSCPATH['graph_end'],[])
 
 
 class MyConnector(Connector):
@@ -139,7 +142,9 @@ class MyConnector(Connector):
                                 self.m_cell0.m_diam/2, self.m_cell1.m_diam/2,
                                 color=self.m_color,
                                 path=self.m_path)
+            self.m_field.m_osc.send_laser(OSCPATH['graph_begin'],["conx",self.m_id])
             self.m_shape.draw()
+            self.m_field.m_osc.send_laser(OSCPATH['graph_end'],[])
 
 
 class MyGroup(Group):

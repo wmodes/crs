319486a0e37c86bc628fc10ec5bf1d8ef4d43186
Wes Modes <wmodes@gmail.com>
2014-09-11 10:57:48 -0700

    Revert "bug fiux"
    This reverts commit 6c738d2a4530ee08f9171e25de3bad1dec4a2e30.

--- conductor/myoschandler.py
+++ conductor/myoschandler.py
@@ -126,7 +126,7 @@ class MyOSCHandler(OSCHandler):
         #print "args:",args,args[0],args[1],args[2]
         #frame = args[0]
         condglobal = args[0]
-        if dbug.LEV & dbug.MSGS: print "OSC:event_ui_condglobal:condglobal =",condglobal
+        if dbug.LEV & dbug.MSGS: print "OSC:event_ui_condglobal:cg =",cg
         self.m_conductor.update(condglobal=condglobal)
 
 
